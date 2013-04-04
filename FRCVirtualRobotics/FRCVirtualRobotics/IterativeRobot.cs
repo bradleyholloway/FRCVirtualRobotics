3970fefe215ad3cc41fabab2442ca3ea1d9078f3
Bradley Holloway <drummerboy400@gmail.com>
2013-04-04 16:38:04 -0500

    Initial Commit

---
+++ FRCVirtualRobotics/FRCVirtualRobotics/IterativeRobot.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace FRC_Virtual_Robotics
+{
+    class IterativeRobot
+    {
+        private double leftMotorSpeed;
+        private double rightMotorSpeed;
+        private double directionForward;
+        private double magnitude;
+        private int scalar;
+        private Texture2D image;
+        private Vector2 location;
+
+        public IterativeRobot(int maxSpeed)
+        {
+            leftMotorSpeed = 0;
+            rightMotorSpeed = 0;
+            scalar = maxSpeed;
+            directionForward = Math.PI * 3 / 2;
+            location = new Vector2(0, 0);
+            image = null;
+        }
+
+        public void run()
+        {
+            magnitude = (leftMotorSpeed + rightMotorSpeed) / 2 * scalar;
+            directionForward += (rightMotorSpeed - leftMotorSpeed);
+            
+            location += magD(magnitude, directionForward);
+        }
+
+        public void setMotorValues(double left, double right)
+        {
+            leftMotorSpeed = left;
+            rightMotorSpeed = right;
+        }
+
+        public Texture2D getImage()
+        {
+            return image;//with rotation
+        }
+
+        public Vector2 getLocation()
+        {
+            return location;
+        }
+
+        private Vector2 magD(double mag, double dir)
+        {
+            int x = (int) (mag * Math.Cos(dir));
+            int y = (int) (mag * Math.Sin(dir));
+            return new Vector2(x,y);    
+        }
+}
---
+++ FRCVirtualRobotics/FRCVirtualRobotics/Utils.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace BradleyXboxUtils
+{
+    public class ControllerInput
+    {
+        PlayerIndex p;
+
+        public ControllerInput()
+        {
+            p = PlayerIndex.One;
+        }
+
+        public ControllerInput(PlayerIndex player)
+        {
+            p = player;
+        }
+
+        public double getLeftX()
+        {
+            return GamePad.GetState(p).ThumbSticks.Left.X;
+        }
+        public double getRightX()
+        {
+            return GamePad.GetState(p).ThumbSticks.Right.X;
+        }
+        public double getLeftY()
+        {
+            return GamePad.GetState(p).ThumbSticks.Left.Y;
+        }
+        public double getRightY()
+        {
+            return GamePad.GetState(p).ThumbSticks.Right.Y;
+        }
+        public Boolean getBottomActionButton()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.A);
+        }
+        public Boolean getLeftActionButton()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.X);
+        }
+        public Boolean getTopActionButton()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.Y);
+        }
+        public Boolean getRightActionButton()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.B);
+        }
+        public Boolean getUpDPad()
+        {
+            return (GamePad.GetState(p).DPad.Up == ButtonState.Pressed);
+        }
+        public Boolean getRightDPad()
+        {
+            return (GamePad.GetState(p).DPad.Right == ButtonState.Pressed);
+        }
+        public Boolean getDownDPad()
+        {
+            return (GamePad.GetState(p).DPad.Down == ButtonState.Pressed);
+        }
+        public Boolean getLeftDPad()
+        {
+            return (GamePad.GetState(p).DPad.Left == ButtonState.Pressed);
+        }
+        public Boolean getStart()
+        {
+            return (GamePad.GetState(p).IsButtonDown(Buttons.Start));
+        }
+        public Boolean getBack()
+        {
+            return (GamePad.GetState(p).IsButtonDown(Buttons.Back));
+        }
+        public Boolean getBigButton()
+        {
+            return (GamePad.GetState(p).IsButtonDown(Buttons.BigButton));
+        }
+        public Boolean getRightBumper()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.RightShoulder);
+        }
+        public Boolean getLeftBumper()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.LeftShoulder);
+        }
+        public Boolean getRightStickDown()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.RightStick);
+        }
+        public Boolean getLeftStickDown()
+        {
+            return GamePad.GetState(p).IsButtonDown(Buttons.LeftStick);
+        }
+        public double getRightTrigger()
+        {
+            return GamePad.GetState(p).Triggers.Right;
+        }
+        public double getLeftTrigger()
+        {
+            return GamePad.GetState(p).Triggers.Left;
+        }
+    }//Controller Input
+
+    public class Toggle
+    {
+        Boolean value;
+        Boolean lastValue;
+
+        public Toggle()
+        {
+            value = false;
+            lastValue = false;
+        }
+        public Toggle(Boolean a)
+        {
+            value = a;
+            lastValue = false;
+        }
+
+        public Boolean update(Boolean a)
+        {
+            if (a != lastValue)
+            {
+                if (a)
+                {
+                    value = !value;
+                    lastValue = true;
+                }
+                else if (!a)
+                {
+                    lastValue = false;
+                }
+            }
+            return value;
+        }
+        public Boolean get()
+        {
+            return value;
+        }
+    }//Toggle
+}
